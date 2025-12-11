8bf3994619a5f7d66d502c0799a14288a30eb103
Andrey Shevandrin <shevandrin@gmail.com>
2025-12-11 20:34:42 +0100

    delete leftovers after reanming

--- botscout/__init__.py
+++
@@ -1,9 +0,0 @@
-__version__ = "0.1.0"
-
-
-from .launcher import launch_page, check_ip
-from ._detector_utils import _find_elements_by_computed_style, _get_html_from_element, _is_element_interactive, \
-    _find_cursor_is_pointer
-from ._launcher_utils import _handle_cookie_consent, _click_element_from_data
-
-print(f"BotScout package initialized (version {__version__})")
