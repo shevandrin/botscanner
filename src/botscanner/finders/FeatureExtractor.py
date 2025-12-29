20a4a8da2cef11b14133937c047a345e9a235ca5
Andrey Shevandrin <shevandrin@gmail.com>
2025-12-29 11:24:38 +0100

    feat: implement FeatureExtractor class for chatbot feature extraction

---
+++ src/botscanner/finders/FeatureExtractor.py
@@ -0,0 +1,42 @@
+from botscanner.models.ChatbotFeatures import ChatbotFeatures, PositionFeature, ResolvedFeature, FeatureCandidate
+from botscanner.evaluators.get_location_chatbot_anchor import get_location_chatbot_anchor
+
+class FeatureExtractor:
+    def __init__(self, driver, ChatbotDetector, logger):
+        self.driver = driver
+        self.anchor = ChatbotDetector.selected_anchor
+        self.window = ChatbotDetector.selected_window
+        self.logger = logger
+
+
+    def extract_anchor_position(self) -> PositionFeature:
+        loc = get_location_chatbot_anchor(self.anchor.element)
+        if loc is not None:
+            self.logger.info(f"Anchor position determined: {loc}")
+            return PositionFeature(
+                sector=loc)
+        else:
+            return PositionFeature(
+                sector=None
+        )
+
+    def extract_window_type(self) -> str:
+        if self.window.context == "iframe":
+            return "iframe"
+        if self.window.context == "shadow":
+            return "shadow_dom"
+        return "dom"
+
+    def extract_title(self) -> ResolvedFeature:
+        candidates = []
+        # TODO: implement title extraction logic
+        return None, None
+
+    def extract(self) -> ChatbotFeatures:
+        return ChatbotFeatures(
+            anchor_position=self.extract_anchor_position(),
+            window_position=None,  # later
+            window_type=self.extract_window_type(),
+            first_visible_text=None,  # later
+            title=self.extract_title(),
+        )
